71b3448cb378eabbb448f92a81109194d4c9c8f1
Adetunji Adegbite <tunjiadegbite@outlook.com>
2019-07-11 05:42:59 +0100

    configured webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// import path from 'path';
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['@babel/preset-env'],
+      },
+    }],
+  },
+};
